f8aebcb690b52607908c487ef17ccf3db5663002
Tim Daubenschütz <tim@ascribe.io>
2015-07-10 14:15:22 +0200

    fix warnings

--- react_s3_fine_uploader.js
+++ react_s3_fine_uploader.js
@@ -24,11 +24,17 @@ var ReactS3FineUploader = React.createClass({
         keyRoutine: React.PropTypes.shape({
             url: React.PropTypes.string,
             fileClass: React.PropTypes.string,
-            pieceId: React.PropTypes.string
+            pieceId: React.PropTypes.oneOfType([
+                React.PropTypes.string,
+                React.PropTypes.number
+            ])
         }),
         createBlobRoutine: React.PropTypes.shape({
             url: React.PropTypes.string,
-            pieceId: React.PropTypes.string
+            pieceId: React.PropTypes.oneOfType([
+                React.PropTypes.string,
+                React.PropTypes.number
+            ])
         }),
         submitKey: React.PropTypes.func,
         autoUpload: React.PropTypes.bool,
@@ -70,7 +76,10 @@ var ReactS3FineUploader = React.createClass({
             customHeaders: React.PropTypes.object
         }).isRequired,
         session: React.PropTypes.shape({
-            endpoint: React.PropTypes.bool
+            customHeaders: React.PropTypes.object,
+            endpoint: React.PropTypes.string,
+            params: React.PropTypes.object,
+            refreshOnRequests: React.PropTypes.bool
         }),
         validation: React.PropTypes.shape({
             itemLimit: React.PropTypes.number,
