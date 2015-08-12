7040007608178586c8d3b6d3e204a3872fdc75ca
Tim Daubenschütz <tim@ascribe.io>
2015-08-12 09:52:51 +0200

    first slide cyland archive submission

--- form.js
+++ form.js
@@ -103,7 +103,6 @@ let Form = React.createClass({
         if ('getFormData' in this.props){
             data = mergeOptionsWithDuplicates(data, this.props.getFormData());
         }
-        console.log(data);
         return data;
     },
 
