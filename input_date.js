a024b2494ece861295b8597881dd0feabb1907dc
Tim Daubenschütz <tim@ascribe.io>
2015-08-24 11:22:44 +0200

    fix PR issues

--- input_date.js
+++ input_date.js
@@ -18,6 +18,9 @@ let InputDate = React.createClass({
         };
     },
 
+    // InputDate needs to support setting a defaultValue from outside.
+    // If this is the case, we need to call handleChange to propagate this
+    // to the outer Property
     componentWillReceiveProps(nextProps) {
         if(!this.state.value && !this.state.value_moment && nextProps.defaultValue) {
             this.handleChange(this.props.defaultValue);
