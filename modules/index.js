df58b8860550ed3f59077448ed003ef530600f5f
Brett Sun <qisheng.brett.sun@gmail.com>
2016-02-10 14:28:12 +0100

    Move ascribe-uploader to modules and add to index.js

--- modules/index.js
+++ modules/index.js
@@ -2,3 +2,10 @@
 
 import 'babel-runtime';
 
+// Uploader
+export ReactS3FineUploader from './react_fine_uploader/react_s3_fine_uploader';
+export ReactFineUploaderUtils from './react_fine_uploader/react_fine_uploader_utils';
+
+// Uploader UI
+export FileDragAndDrop from './react_fine_uploader/file_drag_and_drop/file_drag_and_drop';
+export UploadButton from './react_fine_uploader/upload_button/upload_button';
