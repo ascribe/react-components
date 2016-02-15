ded645e7ddab215a47b31025f21a9b1690ef4c6a
Brett Sun <qisheng.brett.sun@gmail.com>
2016-02-15 12:46:07 +0100

    Add shared utils

--- modules/index.js
+++ modules/index.js
@@ -1,3 +1,4 @@
 import 'babel-runtime';
 
 export * as Uploader from './uploader/index';
+export * as Utils from './utils/index';
---
+++ modules/utils/index.js
@@ -0,0 +1,5 @@
+// Don't bother with exporting the general utils under a namespace
+export * from '.general_utils';
+
+export * as FeatureDetect from './feature_detection';
+export * as File from './file_utils';
