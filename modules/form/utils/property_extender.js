9ee3113018e706571f088cebffad73a717e7b7a5
Brett Sun <qisheng.brett.sun@gmail.com>
2016-03-21 14:34:44 +0100

    Add PropertyExtender to easily expose Property API on custom Properties

---
+++ modules/form/utils/property_extender.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class PropertyExtenderBuilder {
+    constructor(PropertyComponent, propertyRefName = 'property') {
+        this.propertyRefName = propertyRefName;
+
+        // Set up spec for creating extended property class
+        this.spec = {
+            displayName: 'PropertyExtender',
+
+            // Use traditional binding so `this` evaluates to the rendered component
+            render() {
+                return (<PropertyComponent ref="property" {...this.props} />);
+            }
+        };
+    }
+
+    createClass() {
+        return React.createClass(this.spec);
+    }
+
+    extendForFn(fnName) {
+        const { propertyRefName } = this;
+
+        // Use traditional binding so `this` evaluates to the rendered component
+        this.spec[fnName] = function (...args) {
+            const propertyRef = this.refs.property.refs[propertyRefName];
+
+            if (!propertyRef) {
+                throw new Error(`Could not find Property ref (using ${propertyRefName}) for ` +
+                                `extended component (display name: ${PropertyComponent.displayName}) ` +
+                                `when invoking extended function (${fnName}) from Property`);
+            }
+
+            if (typeof propertyRef[fnName] !== 'function') {
+                throw new Error(`Could not find invoked function: ${fnName} in extended Property ` +
+                                `component (display name: ${PropertyComponent.displayName}) ` +
+                                `using Property ref: ${propertyRefName}`);
+            }
+
+             return propertyRef[fnName](...args);
+        }
+    }
+}
+
+const PropertyExtender = (PropertyComponent, propertyRefName) => {
+    const builder = new PropertyExtenderBuilder(PropertyComponent, propertyRefName);
+    builder.extendForFn('handleFocus');
+    builder.extendForFn('handleSubmitFailure');
+    builder.extendForFn('handleSubmitSuccess');
+    builder.extendForFn('reset');
+    builder.extendForFn('validate');
+
+    return builder.createClass();
+};
+
+export default PropertyExtender;
