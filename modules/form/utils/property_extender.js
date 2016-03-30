20ffbcd10b93f7aac059211ca9f330be90e9abca
Brett Sun <qisheng.brett.sun@gmail.com>
2016-03-30 13:09:24 +0200

    Add generic ComponentExtenderBuilder class for creating component extenders

--- modules/form/utils/property_extender.js
+++ modules/form/utils/property_extender.js
@@ -1,63 +1,19 @@
 import React from 'react';
 
-class PropertyExtenderBuilder {
-    constructor(PropertyComponent, propertyRefName = 'property') {
-        this.propertyRefName = propertyRefName;
+import { ComponentExtenderBuilder } from '../../utils/react';
 
-        // Set up spec for creating extended property class
-        this.spec = {
-            displayName: 'PropertyExtender',
+const PropertyExtender = (PropertyComponent, propertyRefName = 'property') => {
+    const builder = new ComponentExtenderBuilder(PropertyComponent, propertyRefName, {
+        displayName: 'PropertyExtender'
+    });
 
-            // Use traditional binding so `this` evaluates to the rendered component
-            render() {
-                return (<PropertyComponent ref="property" {...this.props} />);
-            }
-        };
-    }
-
-    createClass() {
-        return React.createClass(this.spec);
-    }
-
-    extendForFn(fnName) {
-        const { propertyRefName } = this;
-
-        // Use traditional binding so `this` evaluates to the rendered component
-        this.spec[fnName] = function (...args) {
-            const component = this.refs.property;
-
-            if (typeof component[fnName] !== 'function') {
-                const propertyRef = component.refs[propertyRefName];
-
-                if (!propertyRef) {
-                    throw new Error(`Could not find Property ref (using ${propertyRefName}) for ` +
-                                    `extended component (display name: ${PropertyComponent.displayName}) ` +
-                                    `when invoking extended function (${fnName}) from Property`);
-                }
-
-                if (typeof propertyRef[fnName] !== 'function') {
-                    throw new Error(`Could not find invoked function: ${fnName} in extended Property ` +
-                                    `component (display name: ${PropertyComponent.displayName}) ` +
-                                    `using Property ref: ${propertyRefName}`);
-                }
-
-                return propertyRef[fnName](...args);
-            } else {
-                // If the component being extended already has this function, just invoke it
-                return this.refs.property[fnName](...args);
-            }
-        }
-    }
-}
-
-const PropertyExtender = (PropertyComponent, propertyRefName) => {
-    const builder = new PropertyExtenderBuilder(PropertyComponent, propertyRefName);
-    builder.extendForFn('focus');
-    builder.extendForFn('getValue');
-    builder.extendForFn('onSubmitFailure');
-    builder.extendForFn('onSubmitSuccess');
-    builder.extendForFn('reset');
-    builder.extendForFn('validate');
+    builder
+        .extendForFn('focus')
+        .extendForFn('getValue')
+        .extendForFn('onSubmitFailure')
+        .extendForFn('onSubmitSuccess')
+        .extendForFn('reset')
+        .extendForFn('validate');
 
     return builder.createClass();
 };
--- modules/utils/react.js
+++ modules/utils/react.js
@@ -1,20 +1,72 @@
 import React from 'react';
 
 /**
- * Create a new component that always has the given props added to it when invoked
+ * Builder for creating component extenders that propagate methods down to base refs.
  *
- * @param  {function} Component    React component to stuff with props
- * @param  {object}   stuffedProps Props to be always added to the component
- * @return {function}              New React component
+ * Useful when you want to add the same public API from a base component to extended components.
+ *
+ * Constructor:
+ *   @param {function} Component   Component to extend with the builder
+ *   @param {string}   baseRefName Ref name from the extension component to the base
+ *   @param {object}   spec        React class spec used to initialize the extension component.
+ *                                 Usually the only useful thing to provide here is a `displayName`.
+ *
+ * Public API:
+ *   createClass(): Creates the built, extended component
+ *   extendForFn(fnName): Adds fnName to the extended component
+ *
  */
-export function PropStuffer(Component, stuffedProps, displayName = 'PropStuffer') {
-    const StuffedComponent = ({ ...props }) => (
-        <Component {...props} {...stuffedProps} />
-    );
+export class ComponentExtenderBuilder {
+    constructor(Component, baseRefName = 'component', spec = {}) {
+        // The extension component will just render the given component but attach a ref so it can
+        // propagate method calls down the refs.
+        // Use traditional binding so `this.props` evaluates to the rendered component's props
+        spec.render = function () {
+            return (<Component ref="component" {...this.props} />);
+        };
 
-    StuffedComponent.displayName = displayName;
+        this.baseRefName = baseRefName;
+        this.componentDisplayName = Component.displayName;
+        this.spec = spec;
+    }
 
-    return StuffedComponent;
+    createClass() {
+        return React.createClass(this.spec);
+    }
+
+    extendForFn(fnName) {
+        const { baseRefName, componentDisplayName } = this;
+
+        // Add the given function to the extended class
+        // Use traditional binding so `this.refs` evaluates to the rendered component's refs
+        this.spec[fnName] = function (...args) {
+            const { component } = this.refs;
+
+            if (typeof component[fnName] === 'function') {
+                // If the component being extended already has this function, just invoke it
+                return component[fnName](...args);
+            } else {
+                const baseRef = component.refs[baseRefName];
+
+                if (!baseRef) {
+                    throw new Error(`Could not find base ref (using ${baseRefName}) for ` +
+                                    `extended component (display name: ${componentDisplayName}) ` +
+                                    `when invoking extended function (${fnName}) from base`);
+                }
+
+                if (typeof baseRef[fnName] !== 'function') {
+                    throw new Error(`Could not find function (${fnName}) in base ref (using ` +
+                                    `${baseRefName}) from extended component (display name: ` +
+                                    `${componentDisplayName}) when invoked from extended component`);
+                }
+
+                // Invoke the base component's method of the same name
+                return baseRef[fnName](...args);
+            }
+        }
+
+        return this;
+    }
 }
 
 /**
@@ -31,4 +83,21 @@ export function isReactElement(element) {
     // instance of HTMLElement.
     return element && element.render &&
            !(typeof window.HTMLElement === 'function' && element instanceof window.HTMLElement);
+}
+
+/**
+ * Create a new component that always has the given props added to it when invoked
+ *
+ * @param  {function} Component    React component to stuff with props
+ * @param  {object}   stuffedProps Props to be always added to the component
+ * @return {function}              New React component
+ */
+export function PropStuffer(Component, stuffedProps, displayName = 'PropStuffer') {
+    const StuffedComponent = ({ ...props }) => (
+        <Component {...props} {...stuffedProps} />
+    );
+
+    StuffedComponent.displayName = displayName;
+
+    return StuffedComponent;
 }
