0827b64dd79b206357b4cc12110f3ce199f4232c
Brett Sun <qisheng.brett.sun@gmail.com>
2016-05-19 16:45:25 +0200

    Attach ids to child inputs of Properties to enable label linking through htmlFor

--- modules/form/properties/property.js
+++ modules/form/properties/property.js
@@ -320,6 +320,7 @@ const Property = React.createClass({
 
             disabled,
             name,
+            id: name,
 
             // Similar to how the child input's value is controlled using this Property's `value`
             // state, the input's defaultValue is also controlled with the `initialValue` state.
