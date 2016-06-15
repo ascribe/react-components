0bc54def11686218eb6ad2f4e8616924bcaeec4d
Brett Sun <qisheng.brett.sun@gmail.com>
2016-06-15 13:02:28 +0200

    Remove IE8 support from UglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ const prodPlugins = [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
         compress: {
+            screw_ie8: true,
             warnings: false
         },
         output: {
